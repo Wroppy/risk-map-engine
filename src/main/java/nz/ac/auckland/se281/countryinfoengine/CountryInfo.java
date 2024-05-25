0a32337738a7629ceb3956b18eff5eba4b0b9404
Weyman Wong <wxwong806@gmail.com>
2024-05-25 23:46:37 +1200

    Added comments and javadoc to the class

--- src/main/java/nz/ac/auckland/se281/countryinfoengine/CountryInfo.java
+++ src/main/java/nz/ac/auckland/se281/countryinfoengine/CountryInfo.java
@@ -2,12 +2,19 @@ package nz.ac.auckland.se281.countryinfoengine;
 
 import nz.ac.auckland.se281.MessageCli;
 
-/** A class that holds a country's info */
+/** A class that holds a country's info. */
 public class CountryInfo {
   private String name;
   private String continent;
   private int taxFees;
 
+  /**
+   * Sets the class attributes.
+   *
+   * @param name the countries nam
+   * @param continent the continent the country is located
+   * @param taxFees the tax fee of the country
+   */
   public CountryInfo(String name, String continent, int taxFees) {
     this.name = name;
     this.continent = continent;
